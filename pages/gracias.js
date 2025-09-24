fa444df9b4a3f44e685f06da04ef488354c1bb41
LUSTRIX-XXX <raulsb221@gmail.com>
2025-09-24 15:40:17 +0200

    Update gracias.js

--- pages/gracias.js
+++ pages/gracias.js
@@ -2,7 +2,8 @@ export default function Gracias() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🎉 ¡Gracias por registrarte en LUSTRIX!</h1>
-      <p>Revisa tu correo, te hemos enviado el acceso a la app.</p>
+      <p>👉 Revisa tu correo, te hemos enviado el acceso a la aplicación.</p>
     </div>
   );
 }
+
